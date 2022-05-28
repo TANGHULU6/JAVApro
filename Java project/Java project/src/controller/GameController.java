b8b1f07e15dd5b0919266b5f895658bafdc5d94e
TANGHULU6 <106388112+TANGHULU6@users.noreply.github.com>
2022-05-28 12:29:17 +0800

    Add files via upload

---
+++ Java project/Java project/src/controller/GameController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import view.ChessGameFrame;
+import view.Chessboard;
+
+import java.awt.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class GameController {
+    private Chessboard chessboard;
+
+    public GameController(Chessboard chessboard) {
+        this.chessboard = chessboard;
+    }
+
+    public Chessboard getChessboard() {
+        return chessboard;
+    }
+
+    public List<String> loadGameFromFile(String path) {
+        try {
+            List<String> chessData = Files.readAllLines(Path.of(path));
+            chessboard.loadGame(chessData);
+            return chessData;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+//    public Label getstatusLabel(){
+//        return ChessGameFrame.statusLabel;
+//    }
+}
